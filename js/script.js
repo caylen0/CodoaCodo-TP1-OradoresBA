21ea77d630a28c8705ed6eb412bc91b93698a447
Camila Perez <perez9camila@gmail.com>
2022-10-19 11:37:25 -0300

    agrego config de js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//Defino el valor de los tickets
+
+const precio_ticket = 200;
+
+//Porcentaje de descuento
+
+let descuento_estudiante = 80;
+let descuento_trainee = 50;
+let descuento_junior = 15;
