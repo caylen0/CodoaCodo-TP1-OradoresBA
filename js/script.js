14834763622a21d2a647d99fd6c073ef94ac742d
Camila Perez <perez9camila@gmail.com>
2022-10-19 16:51:08 -0300

    optimizo codigo

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ const precio_ticket = 200;
 
 //Porcentaje de descuento
 
-let descuento_estudiante = 80;
-let descuento_trainee = 50;
-let descuento_junior = 15;
+let descuento_estudiante = 0.80;
+let descuento_trainee = 0.50;
+let descuento_junior = 0.15;
 
 //Creo variables necesarias
 
@@ -16,6 +16,11 @@ let total_a_pagar = document.getElementById("total_a_pagar")
 
 //Creo funcion para calcular el total a pagar
 
+function total_pago(total_con_descuento,total_sin_descuento,descuento) {
+    total_con_descuento = total_sin_descuento - (descuento * total_sin_descuento)
+    return total_con_descuento
+}
+
 function total() {
 
     let total_sin_descuento = (cantidad_tickets.value) * precio_ticket;
@@ -24,18 +29,14 @@ function total() {
 
     //Aplico descuento segun categoria
     if (tipo_descuento.value == "estudiante") {
-        total_con_descuento = total_sin_descuento - (descuento_estudiante * total_sin_descuento / 100)
+        total_con_descuento = total_pago(total_con_descuento,total_sin_descuento,descuento_estudiante)
     }
     
     if (tipo_descuento.value == "trainee") {
-        total_con_descuento = total_sin_descuento - (descuento_trainee * total_sin_descuento / 100)
+        total_con_descuento = total_pago(total_con_descuento,total_sin_descuento,descuento_trainee)
     }
 
     if (tipo_descuento.value == "junior") {
-        total_con_descuento = total_sin_descuento - (descuento_junior * total_sin_descuento / 100)
+        total_con_descuento = total_pago(total_con_descuento,total_sin_descuento,descuento_junior)
     }
-}
-
-function mostrar_total () {
-    total_a_pagar.innerHTML = total_con_descuento
 }
